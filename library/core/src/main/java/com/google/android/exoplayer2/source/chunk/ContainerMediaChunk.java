ed7d8e18dbaee5252708e5bd30a05d3cebc1b818
weihanyumo <dhdforever@163.com>
2025-03-07 17:48:35 +0800

    add all files

---
+++ library/core/src/main/java/com/google/android/exoplayer2/source/chunk/ContainerMediaChunk.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.exoplayer2.source.chunk;
+
+import static com.google.android.exoplayer2.util.Assertions.checkNotNull;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.extractor.DefaultExtractorInput;
+import com.google.android.exoplayer2.extractor.Extractor;
+import com.google.android.exoplayer2.extractor.ExtractorInput;
+import com.google.android.exoplayer2.source.chunk.ChunkExtractor.TrackOutputProvider;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DataSourceUtil;
+import com.google.android.exoplayer2.upstream.DataSpec;
+import com.google.android.exoplayer2.util.ConditionVariable;
+import com.google.android.exoplayer2.util.TraceUtil;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+
+/**
+ * A {@link BaseMediaChunk} that uses an {@link Extractor} to decode sample data.
+ *
+ * @deprecated com.google.android.exoplayer2 is deprecated. Please migrate to androidx.media3 (which
+ *     contains the same ExoPlayer code). See <a
+ *     href="https://developer.android.com/guide/topics/media/media3/getting-started/migration-guide">the
+ *     migration guide</a> for more details, including a script to help with the migration.
+ */
+@Deprecated
+public class ContainerMediaChunk extends BaseMediaChunk {
+  private static String TAG = "ContainerMediaChunk";
+
+  private final int chunkCount;
+  private final long sampleOffsetUs;
+  private final ChunkExtractor chunkExtractor;
+
+  private long nextLoadPosition;
+  private volatile boolean loadCanceled;
+  private boolean loadCompleted;
+  private ConditionVariable loadCondition = new ConditionVariable();
+
+  /**
+   * @param dataSource The source from which the data should be loaded.
+   * @param dataSpec Defines the data to be loaded.
+   * @param trackFormat See {@link #trackFormat}.
+   * @param trackSelectionReason See {@link #trackSelectionReason}.
+   * @param trackSelectionData See {@link #trackSelectionData}.
+   * @param startTimeUs The start time of the media contained by the chunk, in microseconds.
+   * @param endTimeUs The end time of the media contained by the chunk, in microseconds.
+   * @param clippedStartTimeUs The time in the chunk from which output will begin, or {@link
+   *     C#TIME_UNSET} to output from the start of the chunk.
+   * @param clippedEndTimeUs The time in the chunk from which output will end, or {@link
+   *     C#TIME_UNSET} to output to the end of the chunk.
+   * @param chunkIndex The index of the chunk, or {@link C#INDEX_UNSET} if it is not known.
+   * @param chunkCount The number of chunks in the underlying media that are spanned by this
+   *     instance. Normally equal to one, but may be larger if multiple chunks as defined by the
+   *     underlying media are being merged into a single load.
+   * @param sampleOffsetUs An offset to add to the sample timestamps parsed by the extractor.
+   * @param chunkExtractor A wrapped extractor to use for parsing the data.
+   */
+  public ContainerMediaChunk(
+      DataSource dataSource,
+      DataSpec dataSpec,
+      Format trackFormat,
+      @C.SelectionReason int trackSelectionReason,
+      @Nullable Object trackSelectionData,
+      long startTimeUs,
+      long endTimeUs,
+      long clippedStartTimeUs,
+      long clippedEndTimeUs,
+      long chunkIndex,
+      int chunkCount,
+      long sampleOffsetUs,
+      ChunkExtractor chunkExtractor) {
+    super(
+        dataSource,
+        dataSpec,
+        trackFormat,
+        trackSelectionReason,
+        trackSelectionData,
+        startTimeUs,
+        endTimeUs,
+        clippedStartTimeUs,
+        clippedEndTimeUs,
+        chunkIndex);
+    Log.d(TAG, "ContainerMediaChunk chunkCount: "+ chunkCount);
+    this.chunkCount = chunkCount;
+    this.sampleOffsetUs = sampleOffsetUs;
+    this.chunkExtractor = chunkExtractor;
+  }
+
+  @Override
+  public long getNextChunkIndex() {
+    return chunkIndex + chunkCount;
+  }
+
+  @Override
+  public boolean isLoadCompleted() {
+    return loadCompleted;
+  }
+
+  // Loadable implementation.
+
+  @Override
+  public final void cancelLoad() {
+//    Log.d(TAG, "cancelLoad: ");
+    loadCondition.open();
+    loadCanceled = true;
+  }
+
+  @SuppressWarnings("NonAtomicVolatileUpdate")
+  @Override
+  public final void load() throws IOException {
+    if (nextLoadPosition == 0) {
+      // Configure the output and set it as the target for the extractor wrapper.
+      BaseMediaChunkOutput output = getOutput();
+      output.setSampleOffsetUs(sampleOffsetUs);
+      chunkExtractor.init(
+          getTrackOutputProvider(output),
+          clippedStartTimeUs == C.TIME_UNSET ? C.TIME_UNSET : (clippedStartTimeUs - sampleOffsetUs),
+          clippedEndTimeUs == C.TIME_UNSET ? C.TIME_UNSET : (clippedEndTimeUs - sampleOffsetUs));
+    }
+    try {
+      // Create and open the input.
+      DataSpec loadDataSpec = dataSpec.subrange(nextLoadPosition);
+      Log.d(TAG, "load position: " + loadDataSpec.position);
+      ExtractorInput input =
+          new DefaultExtractorInput(
+              dataSource, loadDataSpec.position, dataSource.open(loadDataSpec));
+      // Load and decode the sample data.
+      long position = input.getPosition();
+      boolean result = true;
+      loadCondition.open();
+      try {
+        while (result && !loadCanceled) {
+          try {
+            loadCondition.block();
+          } catch (InterruptedException e) {
+            throw new InterruptedIOException();
+          }
+          result = chunkExtractor.read(input);
+          if (!result) {
+            break;
+          }
+          long currentInputPosition = input.getPosition();
+//          Log.d("ContainerMediaChunk", "load currentPos: "+ currentInputPosition + " startPos: "+ position + " delta: "+ (currentInputPosition-position) );
+          if (currentInputPosition > position + 128 * 1024) {
+            position = currentInputPosition;
+//            Log.d(TAG, "loadCondition close: "+ loadCondition);
+            loadCondition.close();
+            TraceUtil.beginSection("askContinue");
+            if (!loadCanceled) {
+              checkNotNull(callback).continueLoadingChunkRequested();
+            }
+            TraceUtil.endSection();
+          }
+          Thread.sleep(1);
+        }
+      } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+      } finally {
+        nextLoadPosition = input.getPosition() - dataSpec.position;
+      }
+    } finally {
+      DataSourceUtil.closeQuietly(dataSource);
+    }
+    loadCompleted = !loadCanceled;
+  }
+
+  public boolean continueLoading(long playbackPositionUs) {
+//    Log.d(TAG, "continueLoading: loadCondition open: "+ loadCondition);
+    return loadCondition.open();
+  }
+
+  /**
+   * Returns the {@link TrackOutputProvider} to be used by the wrapped extractor.
+   *
+   * @param baseMediaChunkOutput The {@link BaseMediaChunkOutput} most recently passed to {@link
+   *     #init(BaseMediaChunkOutput)}.
+   * @return A {@link TrackOutputProvider} to be used by the wrapped extractor.
+   */
+  protected TrackOutputProvider getTrackOutputProvider(BaseMediaChunkOutput baseMediaChunkOutput) {
+    return baseMediaChunkOutput;
+  }
+}
---
+++ library/extractor/src/main/java/com/google/android/exoplayer2/extractor/ESExtractor.java
@@ -0,0 +1,206 @@
+package com.google.android.exoplayer2.extractor;
+
+import android.renderscript.ScriptGroup;
+import android.util.Log;
+import android.util.Pair;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.util.MimeTypes;
+import com.google.android.exoplayer2.util.NalUnitUtil;
+import com.google.android.exoplayer2.util.ParsableByteArray;
+import com.google.android.exoplayer2.util.ParsableNalUnitBitArray;
+
+import org.w3c.dom.NameList;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.util.Arrays;
+
+public class ESExtractor implements Extractor, SeekMap {
+
+    private static final int STATE_READING_ATOM_HEADER = 0;
+    private static final int STATE_READING_ATOM_PAYLOAD = 1;
+    private static final int STATE_READING_SAMPLE = 2;
+    private static final int STATE_READING_SEF = 3;
+    int extractorState = STATE_READING_ATOM_HEADER;
+    private final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    private static final int BUFFER_SIZE = 1024;
+    private long pts = 0;
+
+    int writePos = 0;
+    private ExtractorOutput output;
+    private TrackOutput trackOutput;
+
+    @Override
+    public boolean sniff(ExtractorInput input) throws IOException {
+        ParsableByteArray header = new ParsableByteArray(4);
+        input.peekFully(header.getData(), 0, 4);
+        boolean is264 = isH264(header.getData());
+        return is264;
+    }
+    private boolean isH264(byte[] header) {
+        return header[0] == 0x00 && header[1] == 0x00 && header[2] == 0x00 && header[3] == 0x01;
+    }
+    @Override
+    public void init(ExtractorOutput output) {
+        pts = 0;
+        this.output = output;
+        this.trackOutput = output.track(0, C.TRACK_TYPE_VIDEO);
+        output.seekMap(this);
+        output.endTracks();
+    }
+
+    @Override
+    public int read(ExtractorInput input, PositionHolder seekPosition) throws IOException {
+        byte[] tempBuffer = new byte[BUFFER_SIZE];
+        int bytesRead = input.read(tempBuffer, 0, tempBuffer.length);
+
+        if (bytesRead == C.RESULT_END_OF_INPUT) {
+            if (buffer.size() > 0) {
+                processFrame(buffer.toByteArray());
+                buffer.reset();
+            }
+            return RESULT_END_OF_INPUT;
+        }
+        buffer.write(tempBuffer, 0, bytesRead);
+
+        if (extractorState == STATE_READING_ATOM_HEADER) {
+            if (initializeFormatFromBuffer(buffer.toByteArray())) {
+                extractorState = STATE_READING_ATOM_PAYLOAD;
+            } else {
+                Log.e("TAG", "read: "  );
+                throw new InterruptedIOException();
+            }
+        } else {
+
+            while (true) {
+                byte[] frame = extractFrame(buffer.toByteArray());
+                if (frame == null) {
+                    break;
+                }
+                processFrame(frame);
+            }
+        }
+        return RESULT_CONTINUE;
+
+    }
+    private boolean initializeFormatFromBuffer(byte[] data) {
+        int spsStartIndex = findNalUnit(data, 0, 0x7);
+        int ppsStartIndex = findNalUnit(data, 0, 0x8);
+
+        if (spsStartIndex == -1 || ppsStartIndex == -1) {
+            return false;
+        }
+
+        byte[] sps = extractNalUnit(data, spsStartIndex);
+        byte[] pps = extractNalUnit(data, ppsStartIndex);
+
+        Format format = parseSps(sps);
+        trackOutput.format(format);
+        return true;
+    }
+    private Format parseSps(byte[] spsData) {
+        NalUnitUtil.SpsData sps = NalUnitUtil.parseSpsNalUnit(spsData, 4, spsData.length);
+        return new Format.Builder()
+                .setSampleMimeType(MimeTypes.VIDEO_H264)
+                .setWidth(sps.width)
+                .setHeight(sps.height)
+                .setPixelWidthHeightRatio(sps.pixelWidthHeightRatio)
+                .build();
+    }
+
+    private int findNalUnit(byte[] data, int startIndex,  int nalType) {
+        for (int i = startIndex; i < data.length - 4; i++) {
+            int type = data[i + 4] & 0x1F;
+            int byte0 = data[i];
+            int byte1 = data[i+1];
+            int byte2 = data[i+2];
+            int byte3 = data[i+3];
+
+            if (byte0 == 0x00 && byte1 == 0x00 && byte2 == 0x00 && byte3 == 0x01) {
+                if ( type == nalType || nalType == -1) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private byte[] extractNalUnit(byte[] data, int startIndex) {
+        int endIndex = findNalUnit(data, startIndex+1, -1);
+        if (endIndex == -1) {
+            endIndex = data.length;
+        }
+        return Arrays.copyOfRange(data, startIndex, endIndex);
+    }
+
+    private void processFrame(byte[] frame) {
+        pts = extractTimestamp(frame);
+
+        trackOutput.sampleData(new ParsableByteArray(frame), frame.length);
+        trackOutput.sampleMetadata(
+                pts,
+                C.BUFFER_FLAG_KEY_FRAME,
+                frame.length,
+                0,
+                null);
+    }
+
+    private long extractTimestamp(byte[] frame) {
+        return pts + 33000;
+    }
+
+    private byte[] extractFrame(byte[] data) {
+        int frameEndIndex = findFrameEnd(1,data);
+        if (frameEndIndex == -1) {
+            return null;
+        }
+
+        byte[] frame = Arrays.copyOfRange(data, 0, frameEndIndex);
+        buffer.reset();
+        buffer.write(data, frameEndIndex, data.length - frameEndIndex);
+
+        return frame;
+    }
+
+    private int findFrameEnd(int startPos, byte[] data) {
+        for (int i = startPos; i < data.length - 3; i++) {
+            if (data[i] == 0x00 && data[i + 1] == 0x00 && data[i + 2] == 0x00 && data[i+3] == 0x01) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public void seek(long position, long timeUs) {
+
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+
+
+    @Override
+    public boolean isSeekable() {
+        return false;
+    }
+
+    @Override
+    public long getDurationUs() {
+        return 0;
+    }
+
+    @Override
+    public SeekPoints getSeekPoints(long timeUs) {
+        return null;
+    }
+}
+
+
